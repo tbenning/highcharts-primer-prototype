50b7b2703ab8419a81bb6d9579da4dacf9b36815
Tyler Benning <tbenning@github.com>
2023-03-24 18:39:57 -0700

    first commit

--- src/components/charts/VerticalBarChart.js
+++ src/components/charts/VerticalBarChart.js
@@ -5,48 +5,48 @@ import HighchartsReact from 'highcharts-react-official'
 import getChartTheme from './chartTheme'
 
 function VerticalBarChart() {
-  const theme = useTheme()
-  const chartTheme = getChartTheme(theme.colorScheme)
-  // const chartTheme = typeof theme === 'object' ? getChartTheme(theme.colorScheme) : {}
-  const options = {
-    ...chartTheme,
-    title: {
-      text: undefined,
-    },
-    xAxis: {
-      title: { text: 'Dates' },
-      categories: ['Sept 3', 'Sept 10', 'Sept 17', 'Sept 24'],
-    },
-    yAxis: {
-      title: {
-        text: 'Issues',
-      },
-    },
-    legend: {
-      enabled: false,
-    },
-    chart: {
-        type: 'column',
-        ...chartTheme.chart,
-      },
-    series: [
-        {
-          data: [1, 2, 1, 4],
+    const theme = useTheme()
+    const chartTheme = getChartTheme(theme.colorScheme)
+    // const chartTheme = typeof theme === 'object' ? getChartTheme(theme.colorScheme) : {}
+    const options = {
+        ...chartTheme,
+        title: {
+            text: undefined,
         },
-      ],
-      plotOptions: {
-        column: {
-          borderRadius: 4,
-          animation: false,
+        xAxis: {
+            title: { text: 'Dates' },
+            categories: ['Sept 3', 'Sept 10', 'Sept 17', 'Sept 24'],
         },
-      },
-  }
+        yAxis: {
+            title: {
+                text: 'Issues',
+            },
+        },
+        legend: {
+            enabled: false,
+        },
+        chart: {
+            type: 'column',
+            ...chartTheme.chart,
+        },
+        series: [
+            {
+                data: [1, 2, 1, 4],
+            },
+        ],
+        plotOptions: {
+            column: {
+                borderRadius: 4,
+                animation: false,
+            },
+        },
+    }
 
-  return (
-    <div>
-      <HighchartsReact highcharts={Highcharts} options={options} />
-    </div>
-  )
+    return (
+        <div>
+            <HighchartsReact highcharts={Highcharts} options={options} />
+        </div>
+    )
 }
 
 export default VerticalBarChart
